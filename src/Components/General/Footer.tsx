6abe80ace0f47700b475491db1d4492b6db0b16a
sujade <dnmzsu@gmail.com>
2024-11-15 15:00:28 +0100

    mock footer

--- src/Components/General/Footer.tsx
+++ src/Components/General/Footer.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <div>
-      <span>Sometimes footer, sometimes rockstar.</span>
-    </div>
+    <Row className={'d-flex justify-content-center align-items-center'}>
+      <Col md='auto'>
+        <Link href={'/one'}>About</Link>
+      </Col>
+      <Col md='auto'>
+        <Link href={'/one'}>Organizations</Link>
+      </Col>
+      <Col md='auto'>
+        <Link href={'/one'}>Hospitals</Link>
+      </Col>
+      <Col md='auto'>
+        <Link href={'/one'}>Volunteering</Link>
+      </Col>
+      <Col md='auto'>
+        <Link href={'/one'}>Privacy Policy</Link>
+      </Col>
+    </Row>
   );
 };
 

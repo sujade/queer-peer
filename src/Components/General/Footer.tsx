3b349be9760b692946cd67e697155e70ac19ede5
sujade <dnmzsu@gmail.com>
2024-11-15 15:25:43 +0100

    finish the skeleton for footer

--- src/Components/General/Footer.tsx
+++ src/Components/General/Footer.tsx
@@ -4,33 +4,49 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <Row className={'d-flex justify-content-center align-items-center'}>
-      <Col md='auto'>
-        <Link href={'/about'} className={'fw-bold'}>
-          About
-        </Link>
-      </Col>
-      <Col md='auto'>
-        <Link href={'/partners'} className={'fw-bold'}>
-          Our Partners
-        </Link>
-      </Col>
-      <Col md='auto'>
-        <Link href={'/volunteering'} className={'fw-bold'}>
-          Volunteering
-        </Link>
-      </Col>
-      <Col md='auto'>
-        <Link href={'/donate'} className={'fw-bold'}>
-          Donate
-        </Link>
-      </Col>
-      <Col md='auto'>
-        <Link href={'/privacy-policy'} className={'fw-bold'}>
-          Privacy Policy
-        </Link>
-      </Col>
-    </Row>
+    <>
+      <Row className={'d-flex justify-content-center align-items-center'}>
+        <Col md='auto'>
+          <Link href={'/about'} className={'fw-bold'}>
+            About
+          </Link>
+        </Col>
+        <Col md='auto'>
+          <Link href={'/partners'} className={'fw-bold'}>
+            Our Partners
+          </Link>
+        </Col>
+        <Col md='auto'>
+          <Link href={'/volunteering'} className={'fw-bold'}>
+            Volunteering
+          </Link>
+        </Col>
+        <Col md='auto'>
+          <Link href={'/donate'} className={'fw-bold'}>
+            Donate
+          </Link>
+        </Col>
+        <Col md='auto'>
+          <Link href={'/privacy-policy'} className={'fw-bold'}>
+            Privacy Policy
+          </Link>
+        </Col>
+      </Row>
+      <Row className={'pt-2'}>
+        <Col>
+          <span
+            className={
+              'fw-bold text-muted d-flex justify-content-center align-items-center'
+            }>
+            made with&nbsp;<span className={'text-danger'}>♥</span>&nbsp; by&nbsp;
+            <Link className={'text-muted'} href={'https://github.com/sujade'}>
+              su jade
+            </Link>
+          </span>
+        </Col>
+      </Row>
+      ;
+    </>
   );
 };
 

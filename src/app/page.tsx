574cf18af5314442048bfe0ef251aff3878e7b30
sujade <dnmzsu@gmail.com>
2025-01-06 11:49:18 +0100

    try new design pattern

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import GetStartedButton from '@/Components/Landing/GetStartedButton';
-import ExplanationText from '@/Components/Landing/ExplanationText';
 
 const Page = () => {
   return (
-    <Container className='d-flex align-items-center justify-content-center my-5'>
-      <Row className='d-flex flex-column align-items-center justify-content-center'>
-        <Col xs={12} className='text-center mb-4'>
-          <ExplanationText />
-        </Col>
+    <Container className='my-5'>
+      <Row className='mb-4'>
         <Col xs={12} className='text-center'>
-          <GetStartedButton />
+          <span>Big title!</span>
+          <br />
+          <span>Let me get you up to speed</span>
+        </Col>
+      </Row>
+      <Row className='mb-4'>
+        <Col xs={6} className='text-center'>
+          <span>Button for patient</span>
+        </Col>
+        <Col xs={6} className='text-center'>
+          <span>Button for organization</span>
         </Col>
       </Row>
     </Container>

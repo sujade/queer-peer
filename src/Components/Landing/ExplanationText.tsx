69e1446a3ae93e4a1c84b81856cdd54992e8b3bd
sujade <dnmzsu@gmail.com>
2025-01-06 11:39:10 +0100

    remove greetingtext due to instabilities

--- src/Components/Landing/ExplanationText.tsx
+++ src/Components/Landing/ExplanationText.tsx
@@ -4,7 +4,7 @@ import { Row } from 'react-bootstrap';
 const ExplanationText = () => {
   return (
     <Row>
-      <h1 className={'explanation-header lh-1'}>What is QueerPeer?</h1>
+      <h1 className={'explanation-header lh-1'}>QueerPeer</h1>
       <span>
         Text about QueerPeer, what it does, how it works and that it is
         non-profit.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import GreetingText from '@/Components/Landing/GreetingText';
 import GetStartedButton from '@/Components/Landing/GetStartedButton';
+import ExplanationText from '@/Components/Landing/ExplanationText';
 
 const Page = () => {
   return (
     <Container className='d-flex align-items-center justify-content-center my-5'>
       <Row className='d-flex flex-column align-items-center justify-content-center'>
-        <Col xs={12}>
-          <GreetingText />
+        <Col xs={12} className='text-center mb-4'>
+          <ExplanationText />
         </Col>
         <Col xs={12} className='text-center'>
           <GetStartedButton />

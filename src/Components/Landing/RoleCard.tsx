bea3602f9d19d45933777f27b6ea2d7a6351858d
sujade <dnmzsu@gmail.com>
2025-01-08 09:39:37 +0100

    add borders and roundness to cards

--- src/Components/Landing/RoleCard.tsx
+++ src/Components/Landing/RoleCard.tsx
@@ -10,9 +10,9 @@ interface RoleCardProps {
 
 const RoleCard: React.FC<RoleCardProps> = ({ title, text, buttonText }) => {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Body className='p-5 d-flex flex-column justify-content-center align-items-center'>
-        <Card.Title className='text-center fs-2'>{title}</Card.Title>
+    <Card className={'border-0'} style={{ width: '18rem' }}>
+      <Card.Body className='border border-black rounded-5 p-5 d-flex flex-column justify-content-center align-items-center'>
+        <Card.Title className='px-5 text-center fs-2'>{title}</Card.Title>
         <Card.Text className='text-center'>{text}</Card.Text>
         <Button variant='primary'>{buttonText}</Button>
       </Card.Body>
